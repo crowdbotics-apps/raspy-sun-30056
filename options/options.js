2e193d8ad3c480d58ce9f1541c34d12fa6ae2c84
Crowdbotics <team@crowdbotics.com>
2021-08-23 21:52:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "raspy_sun_30056Identifier",
+  url: "https://raspy_sun_30056Identifier.botics.co",
+  api: "https://raspy_sun_30056Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
